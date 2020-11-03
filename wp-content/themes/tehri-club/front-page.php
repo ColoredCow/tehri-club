ff0a23e9826a751902150778bd999f22e48ecd25
satendra-sr <officialsatendrarawat@gmail.com>
2020-11-03 16:21:37 +0530

    Some css changes

--- wp-content/themes/tehri-club/front-page.php
+++ wp-content/themes/tehri-club/front-page.php
@@ -19,8 +19,8 @@ get_header();
 						the_post();
 				?>
 						<div class="item w-100p p-0">
-							<div class="blog-entry w-100p p-3 m-0">
-								<img class="w-100p" src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>" />
+							<div class="blog-entry rounded-6 w-100p p-3 m-0">
+								<img class="w-100p rounded-6" src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>" />
 							</div>
 						</div>
 				<?php 	}

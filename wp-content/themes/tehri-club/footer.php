0aae8a5ba1bfe533859ff5c0bfa65fe895ec4326
satendra-sr <officialsatendrarawat@gmail.com>
2020-11-03 16:34:12 +0530

    Footer updates

--- wp-content/themes/tehri-club/footer.php
+++ wp-content/themes/tehri-club/footer.php
@@ -8,7 +8,7 @@
 ?>
 	<?php wp_footer(); ?>
 	<div class="d-flex justify-content-center my-7">
-		<span class="fz-20"> Crafted By <a href="https://coloredcow.com/?utm_source=tehri_club&utm_medium=website" class="text-theme-forestgreen">ColoredCow Tehri</a>  With Love </span>
+		<span class="fz-20"> Crafted By <a href="https://coloredcow.com/?utm_source=tehri_club&utm_medium=website" class="text-theme-forestgreen">ColoredCow Tehri</a>  With <i class="fa fa-heart text-theme-thunderbird" aria-hidden="true"></i> </span>
 	</div>
 </body>
 </html>
--- wp-content/themes/tehri-club/header.php
+++ wp-content/themes/tehri-club/header.php
@@ -6,24 +6,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1">
     <title><?php echo bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <header>
-	<!-- <div class="mobile-menu position-fixed h-75 w-100p text-center bg-white pl-0 d-none flex-column d-xl-block font-primary fz-18 leading-22 pt-14 pt-md-15 justify-content-center">
-		<?php
-			wp_nav_menu(array(
-				'theme_location' => 'header_nav',
-				'container' => 'ul',
-				'menu_class' => 'list-unstyled my-0 mobile-primary-menu w-100p',
-				'echo' => true,
-			));
-		?>
-	</div> -->
 	<div class="p-6">
 		<div class="company-logo">
 			<h2 >ColoredCow <span class="text-theme-forestgreen">Tehri</span> <span class="lighter">club</span></h2>
 		</div>
 	</div>
 </header>
-
 <body>
